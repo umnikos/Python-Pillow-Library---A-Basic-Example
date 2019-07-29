dc38efc20c4b5b797ec115f42e0677e87f9967f6
umnikos <alex.stefanov2@gmail.com>
2019-07-29 15:17:20 +0300

    Add comments to code

--- pillow.py
+++ pillow.py
@@ -2,14 +2,17 @@
 
 from PIL import Image
 
-size = (500,433)
-im = Image.new("1",size, 0)
-px = im.load()
+size = (500,433) # the size of the final image
+im = Image.new("1",size,0) # create a new black and white image
+px = im.load() # create a pixel access object
 
+# takes a 4-tuple range of where to draw the sierpinski triangle
 def triangle(xy, fill=1, depth=5):
+    # if recursion is over, fill the top left pixel
     if depth == 0:
         px[xy[0],xy[1]] = fill
         return
+    # otherwise, draw 3 smaller sierpinski triangles
     triangle(((3*xy[0]+xy[2])//4, 
                xy[1],
                (xy[0]+3*xy[2])//4,
@@ -29,7 +32,7 @@ def triangle(xy, fill=1, depth=5):
               fill,
               depth-1)
 
-triangle((0,0)+size, depth=10)
-im.show()
-im.save("output.png","PNG")
+triangle((0,0)+size, depth=10) # draw the triangle (smaller depths results in a dotty output)
+im.save("output.png","PNG") # save the image
+im.show() # open the image
 
