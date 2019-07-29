e3a7a3ba1a559e96ff4cf9b538d83c5b9c2657ba
umnikos <alex.stefanov2@gmail.com>
2019-07-29 14:54:07 +0300

    Add code

---
+++ pillow.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from PIL import Image
+
+size = (500,433)
+im = Image.new("1",size, 0)
+px = im.load()
+
+def triangle(xy, fill=1, depth=5):
+    if depth == 0:
+        px[xy[0],xy[1]] = fill
+        return
+    triangle(((3*xy[0]+xy[2])//4, 
+               xy[1],
+               (xy[0]+3*xy[2])//4,
+               (xy[1]+xy[3])//2), 
+               fill,
+               depth-1)
+    triangle((xy[0],
+              (xy[1]+xy[3])//2, 
+              (xy[0]+xy[2])//2,
+              xy[3]),
+              fill,
+              depth-1)
+    triangle(((xy[0]+xy[2])//2,
+              (xy[1]+xy[3])//2,
+              xy[2],
+              xy[3]),
+              fill,
+              depth-1)
+
+triangle((0,0)+size, depth=10)
+im.show()
+
